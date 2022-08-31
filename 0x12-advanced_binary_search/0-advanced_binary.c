46ee3a2e975065d449ed0ad46ce6fd3832375e4c
angel.palma <angel.palma@theapescompany.com>
2022-08-31 16:10:20 -0500

    betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,46 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * advanced_binary - binary search
- * @array: array to search
- * @size: size of array
- * @value: value to search for
- *
- * Return: index of value, -1 if not found
- */
-int advanced_binary(int *array, size_t size, int value)
+* find_index - finds the index of the 1st occurrence of a value in an array
+*@an_array:    (int*)  sorted array of integers
+*@start:       (int)   index indicating the start of the sub-array
+*@end:         (int)   index indicating the end of the sub-array
+*@val:         (int)   value to be finded
+*Return:		pos:	(int)   index where value is located or -1 otherwise
+*/
+
+int find_index(int *an_array, size_t start, size_t end, int val)
 {
-	if (array == NULL || size < 1)
+	size_t i, index, pos, len;
+
+	if (start == end)
 		return (-1);
 
-	return (binary_search(array, 0, size - 1, value));
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+	{
+		printf("%d", an_array[i]);
+		if (i < end - 1)
+			printf(", ");
+	}
+	printf("\n");
+
+	pos = index = (start + end - 1) / 2;
+	len = end - start;
+
+	if (len > 0 && (an_array[index] == val) && (an_array[index - 1] > val))
+		pos = index;
+
+	else if ((an_array[index] == val) && (an_array[index - 1] == val))
+		pos = find_index(an_array, start, index + 1, val);
+
+	else if (an_array[index] < val)
+		pos = find_index(an_array, index + 1, end, val);
+
+	else if (an_array[index] > val)
+		pos = find_index(an_array, start, index + 1, val);
+
+	else if (an_array[index] == val)
+		pos = index;
+
+	else
+		pos = -1;
+
+	return (pos);
 }
 
-int binary_search(int *array, int left, int right, int value)
+/**
+ * advanced_binary - Finds an item and its position in an sorted array
+ * @array:  (int)       Array of elements
+ * @size:   (size_t)    Lenght of the array
+ * @value:  (int)       value to be located in the array
+  * Return:	@pos:    (int)       index position if found or -1 otherwise
+ */
+
+int advanced_binary(int *array, size_t size, int value)
 {
-	int mid = (left + right) / 2;
-	int i;
+	int index;
 
-	if (left > right)
-		return (-1);
-	else
-		printf("Searching in array: ");
-
-	for (i = left; i <= right; i++)
-	{
-		if (i < right)
-			printf("%d, ", array[i]);
-		else
-			printf("%d\n", array[i]);
-	}
-
-	if (array[left] == value)
-		return (left);
-
-	if (array[mid] < value)
-		return (binary_search(array, mid + 1, right, value));
-
-	return (binary_search(array, left, mid, value));
+	index = (!array || size < 1) ? -1 : find_index(array, 0, size, value);
+	return (index);
 }
